f845dbe997e365c649c22828e71d18dc1a9bafa0
Jason's Snail <Snail@danger-consultant.com>
2022-01-29 17:42:50 +0800

    update for redis

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import time
+import redis
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/<x>")
+def hello_world(x):
+
+    r = redis.Redis(host='localhost', port=6379, db=0)
+
+    if r.exists(x) == 1:
+        return r.get(x)
+    else:
+        result = x + 1
+        time.sleep(5)
+        r.set(x, result, ex=30)
+        return result
+
+
+
+    #
+    #
+    # #TODO: Ask Redis, does x exist?
+    # r = redis.Redis(host='localhost', port=6379, db=0)
+    #
+    # #TODO: If exist, return stored value
+    #
+    # #TODO: If not exist, calculate
+    # for i in range(100000000):
+    #     result = x + 1
+    #
+    # #TODO: Save result to redis
+    #
+    # return result
